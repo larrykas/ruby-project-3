d7c039dd7dd59500394a4c2616b49e08a8a4945a
larrykas <larrykas@users.noreply.github.com>
2013-09-26 13:13:06 +0300

    Create make_models.rb
    File System and Data Parsing

---
+++ make_models.rb
@@ -0,0 +1,32 @@
+# Instantiate a file object
+file = File.new('makes_models.csv', 'w') 
+
+file.puts '"Make:Chevrolet Model:Malibu"'
+file.puts '"Make:Ford Model:Mustang"'
+file.puts '"Make:Ford Model:Taurus"'
+file.puts '"Make:Chevrolet Model:Cobalt"'
+
+file.close
+
+file = File.open('makes_models.csv', 'r') 
+
+if file = file.sysread(200) 
+  
+  puts file
+
+else
+  puts "Unable to open file!"
+end
+
+File.open('makes_models.csv', 'r') do |file|
+  lines = 0
+  file.each_line do |line|
+    lines += 1
+  end
+  puts "Lines Processed: #{lines}"
+end
+
+
+size = File.open('makes_models.csv', 'r').size
+print "File Size:" 
+puts size 
