5a3f04b764d043655741439a0d5a753d4672c705
larrykas <larrykas@users.noreply.github.com>
2013-09-26 06:48:01 +0300

    Create basic_io.rb
    File System and Data Parsing

---
+++ basic_io.rb
@@ -0,0 +1,34 @@
+
+# Instantiate a file object
+file = File.new('file1.txt', 'w') 
+
+# Writes “Hello World” to the file created above
+file.puts'"Hello World"'
+
+# Always close the file at the end when you use File.new
+file.close
+
+# Reads the text file created above and prints it’s contents to the screen
+file = File.new('file1.txt', 'r') 
+
+# if the mode(rights) of the file above is not read the following code will not be executed, returns an error
+if file
+   content = file.sysread(20) 
+  puts content
+else
+  puts "Unable to open file!"
+end
+
+# On a new line, Appends the following to "Hello World"
+puts "It's going to be a great day"
+
+# Creates a new directory called “my_new_directory”if it doesn't already exist
+if Dir.mkdir('my_new_directory') != nil
+  
+  puts "Yes! File created"
+  
+else
+  
+  puts "This File Already Exists"
+
+end
